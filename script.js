9946b140a359d227d886d4c4aa62c67231e3c49e
Rohit Thirumala <86196249+xrinlix@users.noreply.github.com>
2024-08-24 02:47:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
+            })
+        }
+    })
+}
+
+
+const coords = {x: 0, y: 0};
+const circles = document.querySelectorAll(".circle");
+
+
+circles.forEach(function (circle, index){
+    circle.x = 0;
+    circle.y = 0;
+});
+
+
+
+window.addEventListener("mousemove", function(e){
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+
+    
+   
+});
+
+function animateCircles(){
+
+    let x = coords.x;
+    let y = coords.y;
+
+    circles.forEach(function (circle, index){
+        circle.style.left = x - 12 + "px";
+        circle.style.top = y - 12+ "px";
+
+        circle.style.scale = (circles.length - index) / circles.length;
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.3;
+        y += (nextCircle.y - y) * 0.3;
+    });
+
+    requestAnimationFrame(animateCircles);
+}    
+
+menuIcon.oneclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+animateCircles();
